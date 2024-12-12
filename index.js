d886e3c2dbadc75fe8c4ef76c7999d83ca6a6637
Chander Prakash <cprakash@gmx.de>
2024-12-12 21:55:51 +0100

    Fixed file writing with result

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const path = require("path")
 let file_types = [".js"];
 let max_depth = 1;
 let output_format = [];
-let root = "./"
+let root = "./";
+
+let final_result = {
+    file_name: 0,
+    lines_of_code : 0
+};
+
+let final_results = [];
+let output_file_name = "result"; 
 
 function read_config_file()
 {
@@ -19,6 +27,9 @@ function read_config_file()
         file_types = config_obj['file-types'];
         max_depth = config_obj['max-depth'];
         output_format = config_obj['output-format'];
+
+        output_file_name = output_file_name  + output_format;
+
         root = config_obj['root'];
         console.log("File type= " + file_types + " max depth= " + max_depth + " output format=" + output_format + " root=" + root);
     }
@@ -42,12 +53,16 @@ function read_single_file(file_path)
             return;
         }
     const result = fs.readFileSync(file_path, "utf-8");
-    console.log("|----------------------------------------------|------------------------------|");
+    //console.log("|----------------------------------------------|------------------------------|");
     if(result)
     {
         let out=[];
         out = result.split("\n");
-        console.log("|" + file_path + "\t\t\t\b|\t\t" +out.length+"\t\t\b\b|");
+        final_result = {};
+        final_result.file_name = file_path;
+        final_result.lines_of_code = out.length;
+        final_results.push(final_result);
+        //console.log("|" + file_path + "\t\t\t\b|\t\t" +out.length+"\t\t\b\b|");
     }
     else 
     {
@@ -62,16 +77,27 @@ function start_script()
     if(!file_list) 
     {
         console.log("nothing found at " + root);
+        return;
     }
     else
     {
-        console.log(file_list);
+        //console.log(file_list);
     }
-    console.log("|----------------------------------------------|------------------------------|");
+    //console.log("|----------------------------------------------|------------------------------|");
     file_list.forEach(element => {
         read_single_file(root + element);
     });
-    console.log("|----------------------------------------------|------------------------------|");
+    const output_file = fs.writeFileSync(path.join("./", output_file_name), JSON.stringify(final_results));
+    if (output_file)
+    {
+        console.log("File could not be written, Please try again.");
+    }
+    else 
+    {
+        console.log("File written successfully.");
+    }
+    //console.log(final_results);
+    //console.log("|----------------------------------------------|------------------------------|");
     //read_single_file();
     
 }
